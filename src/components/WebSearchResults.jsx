9186615bf55c5c41090330caa2515842ee34baf8
hoang0106 <nguyen.d.hoang@gmail.com>
2023-08-22 20:38:35 -0700

    adjust desktop layout

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
-import React from 'react';
 import Parser from 'html-react-parser';
 import PaginationButtons from './PaginationButtons';
 
 export default function WebSearchResults({ results }) {
 	return (
-		<div className="w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-[52%]">
+		<div className="w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52">
 			<p className="text-gray-600 text-sm mb-5 mt-3">
-				About {results.searchInformation?.formattedTotalResults} results(
+				About {results.searchInformation?.formattedTotalResults} results (
 				{results.searchInformation?.formattedSearchTime} seconds)
 			</p>
 			{results.items?.map((result) => (
@@ -26,9 +25,7 @@ export default function WebSearchResults({ results }) {
 					<p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
 				</div>
 			))}
-			<div>
-				<PaginationButtons />
-			</div>
+			<PaginationButtons />
 		</div>
 	);
 }
